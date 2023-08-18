f91a71b25728fbfce03e0ee6882b62b0cd556038
Joel Cross <ukch@users.noreply.github.com>
2023-08-18 11:43:17 +0200

    Fix: Use Picker from react-native instead of @react-native-picker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React, { PureComponent } from 'react';
-import { Keyboard, Modal, Platform, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import {
+    Keyboard,
+    Modal,
+    Picker,
+    Platform,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+} from 'react-native';
 import PropTypes from 'prop-types';
 import isEqual from 'lodash.isequal';
-import { Picker } from '@react-native-picker/picker';
 import { defaultStyles } from './styles';
 
 export default class RNPickerSelect extends PureComponent {
